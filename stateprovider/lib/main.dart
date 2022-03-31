a2da185743160a1bf3e45296d44423b70e9f0aca
thanhbui120501 <thanhbui120501@gmail.com>
2022-03-31 09:21:33 +0700

    add ref.listen

--- stateprovider/lib/main.dart
+++ stateprovider/lib/main.dart
@@ -9,9 +9,8 @@ void main() {
   ));
 }
 
-final valueProvider = Provider<int>((ref) {
-  return 36;
-});
+final valueProvider = Provider<int>((ref) => 36);
+final counterProvider = StateProvider((ref) => 0);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -23,18 +22,22 @@ class MyApp extends StatelessWidget {
 }
 
 /// Providers are declared globally and specifies how to create a state
-final counterProvider = StateProvider((ref) => 0);
 
 class Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<StateController<int>>(counterProvider.state,
+        (previous, current) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Value is: ${counterProvider.state}')));
+    });
     return Scaffold(
       appBar: AppBar(title: const Text('Counter example')),
       body: Center(
         // Consumer is a widget that allows you reading providers.
         // You could also use the hook "ref.watch(" if you uses flutter_hooks
         child: Consumer(builder: (context, ref, _) {
-          final count = ref.watch(counterProvider.state).state;
+          final count = ref.watch(counterProvider);
           return Text('$count');
         }),
       ),
